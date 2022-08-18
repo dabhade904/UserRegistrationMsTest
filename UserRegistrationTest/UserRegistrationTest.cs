26c73088361bd420d84985ac0e6b9d00c0d180c7
dabhade904 <dabhade904@gmail.com>
2022-08-18 18:07:32 +0530

    Added : function and test case for validate first name

--- UserRegistrationTest/UserRegistrationTest.cs
+++ UserRegistrationTest/UserRegistrationTest.cs
@@ -4,6 +4,42 @@ namespace ValidateUserRegistration
     [TestClass]
     public class UserRegistrationTest
     {
-     
+        /// <summary>
+        /// Test cases for First Name
+        /// </summary>
+        [TestMethod]
+        public void WhenGivenFirstNameStartWithUppercaseReturnTrue()
+        {
+            bool firstName = UserPattern.ValidateFirstAndLastName("Sachin");
+            Assert.AreEqual(firstName, true);
+        }
+
+        [TestMethod]
+        public void WhenGivenFirstNameStartLowerCaseShouldReturnFalse()
+        {
+            bool firstName = UserPattern.ValidateFirstAndLastName("sachin");
+            Assert.AreEqual(firstName, false);
+        }
+
+        [TestMethod]
+        public void whenGivenFirstName_WithSpecialSymbol_ShouldReturnFalse()
+        {
+            bool firstName = UserPattern.ValidateFirstAndLastName("#achin");
+            Assert.AreEqual(firstName, false);
+        }
+
+        [TestMethod]
+        public void WhenGivenFirstName_WithNumber_ShouldReturnFalse()
+        {
+            bool firstName = UserPattern.ValidateFirstAndLastName("123achin");
+            Assert.AreEqual(firstName, false);
+        }
+
+        [TestMethod]
+        public void WhenGivenFirstName_IsLessThanThreeCharacter()
+        {
+            bool firstName = UserPattern.ValidateFirstAndLastName("Si");
+            Assert.AreEqual(firstName, false);
+        }
     }
 }
